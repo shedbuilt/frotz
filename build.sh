66d7518bee9a910ef2414339a3a0298616c8f740
Auston Stewart <auston@shedbuilt.net>
2018-01-20 07:00:11 -1000

    Updating frotz to use newer shedmake git refs

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Disable sound
-sed -i "s/SOUND = ao/#SOUND = ao/g" Makefile
-make -j $SHED_NUMJOBS
-make "DESTDIR=${SHED_FAKEROOT}" install
+sed -i "s/SOUND = ao/#SOUND = ao/g" Makefile && \
+make -j $SHED_NUMJOBS && \
+make DESTDIR="$SHED_FAKEROOT" install
