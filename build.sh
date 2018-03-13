1948b0a8fe3be9350aa29f3afcaf221440408403
Auston Stewart <auston@shedbuilt.net>
2018-03-12 18:33:35 -1000

    Updating packaging

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Disable sound
-sed -i "s/SOUND = ao/#SOUND = ao/g" Makefile && \
-make -j $SHED_NUMJOBS && \
+make SOUND=none -j $SHED_NUMJOBS &&
 make DESTDIR="$SHED_FAKEROOT" install
